2d9fa3f1d876b18b33b0de92e67e3e48ac46e51e
OortCloud42 <53982755+OortCloud42@users.noreply.github.com>
2022-01-20 15:25:16 +0100

    add the other patch

--- x.c
+++ x.c
@@ -19,6 +19,7 @@ char *argv0;
 #include "arg.h"
 #include "st.h"
 #include "win.h"
+#include "hb.h"
 
 /* types used in config.h */
 typedef struct {
@@ -1062,6 +1063,9 @@ xunloadfont(Font *f)
 void
 xunloadfonts(void)
 {
+    /* Clear Harfbuzz font cache. */
+    hbunloadfonts();
+
 	/* Free the loaded fonts in the font cache.  */
 	while (frclen > 0)
 		XftFontClose(xw.dpy, frc[--frclen].font);
@@ -1368,6 +1372,10 @@ xmakeglyphfontspecs(XftGlyphFontSpec *specs, const Glyph *glyphs, int len, int x
 		numspecs++;
 	}
 
+    /* Harfbuzz transformation for ligatures. */
+    hbtransform(specs, glyphs, len, x, y);
+
+
 	return numspecs;
 }
 
@@ -1517,14 +1525,17 @@ xdrawglyph(Glyph g, int x, int y)
 }
 
 void
-xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og)
+xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og, Line line, int len)
 {
 	Color drawcol;
 
 	/* remove the old cursor */
 	if (selected(ox, oy))
 		og.mode ^= ATTR_REVERSE;
-	xdrawglyph(og, ox, oy);
+
+       /* Redraw the line where cursor was previously.
+        * It will restore the ligatures broken by the cursor. */
+       xdrawline(line, 0, oy, len);
 
 	if (IS_SET(MODE_HIDE))
 		return;
